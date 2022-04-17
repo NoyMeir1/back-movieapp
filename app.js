11e612214b613289ac00c4d5184dc8a273909291
NoyMeir1 <76658178+NoyMeir1@users.noreply.github.com>
2022-04-18 00:21:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(cors())
 
